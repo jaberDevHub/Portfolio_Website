87ffed39f2d7716b3c894271e9477d9981103f11
Jaber Ahmed <121017046+jaberDevHub@users.noreply.github.com>
2026-01-14 22:16:50 +0600

    Delete .history directory

--- .history/fix2_20260113185344.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "(Get-Content index.html) -replace 'class=\"border border-gray-600/30 rounded-lg p-2 text-\[13px\] w-\[100px\] flex justify-center items-center', 'class=\"border border-gray-600/30 rounded-lg p-2 text-[13px] w-[100px] flex justify-center items-center hover:scale-105 transition-transform cursor-pointer\"' | Set-Content index.html"
